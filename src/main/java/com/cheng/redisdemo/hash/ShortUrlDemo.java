b7e9c3060e8fedd5d0dca39be1b3b3a381fc18d4
cheng <chengjj@ihdou.com>
2020-11-13 17:56:24 +0800

    短连接，代办事宜

--- src/main/java/com/cheng/redisdemo/hash/ShortUrlDemo.java
+++ src/main/java/com/cheng/redisdemo/hash/ShortUrlDemo.java
@@ -1,5 +1,7 @@
 package com.cheng.redisdemo.hash;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * @Date:2020/11/13 14:33
  * @Author: Cheng
@@ -7,4 +9,49 @@ package com.cheng.redisdemo.hash;
  */
 public class ShortUrlDemo {
 
+    private Jedis jedis;
+
+    public ShortUrlDemo(){
+        jedis = new Jedis("127.0.0.1");
+        jedis.set("short_url_seed","123456789");
+    }
+
+    private static final String[] X36_ARRAY = "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(",");
+
+    public static void main(String[] args) {
+        ShortUrlDemo demo = new ShortUrlDemo();
+
+        String shortUrl = demo.getShortUrl();
+        System.out.println(shortUrl);
+
+        for (int i = 0; i < 100; i++) {
+            demo.increatShortUrl(shortUrl);
+        }
+
+        long count = demo.getShortUrlCount(shortUrl);
+        System.out.println(count);
+    }
+
+    private long getShortUrlCount(String shortUrl) {
+        return Long.valueOf(jedis.hget("short_url_access_count",shortUrl));
+    }
+
+    private void increatShortUrl(String shortUrl) {
+        jedis.hincrBy("short_url_access_count",shortUrl,1);
+    }
+
+    private String getShortUrl() {
+        long shortUrlSeed = jedis.incr("short_url_seed");
+
+        StringBuilder sb = new StringBuilder();
+        while (shortUrlSeed>0){
+            sb.append(X36_ARRAY[(int)(shortUrlSeed%36)]);
+            shortUrlSeed/=36;
+        }
+
+        String shortUrl = sb.reverse().toString();
+        jedis.hincrBy("short_url_access_count",shortUrl,1);
+        jedis.hincrBy("url_mapper",shortUrl,1);
+        return shortUrl;
+    }
 }
