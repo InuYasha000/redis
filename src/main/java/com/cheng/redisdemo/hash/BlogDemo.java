52a51bd81fbccff90b6389acb78151b94211d108
cheng <chengjj@ihdou.com>
2020-12-30 18:59:07 +0800

    redis的hash用法：短连接生成，博客点赞，预览，浏览次数。内容长度

--- src/main/java/com/cheng/redisdemo/hash/BlogDemo.java
+++ src/main/java/com/cheng/redisdemo/hash/BlogDemo.java
@@ -2,71 +2,88 @@ package com.cheng.redisdemo.hash;
 
 import redis.clients.jedis.Jedis;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
- * @Date:2020/11/12 16:32
+ * @Date:2020/12/30 18:40
  * @Author: Cheng
  * @Description:博客
  */
 public class BlogDemo {
 
+    private Jedis jedis = new Jedis("127.0.0.1");
 
-    private static Jedis jedis = new Jedis("127.0.0.1");
+    /**
+     * 获取blog id
+     *
+     * @return
+     */
+    public long getBlogId() {
+        return jedis.incr("blog_id_counter");
+    }
+
+    /**
+     * 发表一篇博客
+     *
+     * @param title
+     * @param author
+     * @param time
+     * @param content
+     */
+    public void publishBlogResult(String title, String author, String time, String content, int id) {
+        Long publishBlogResult = jedis.msetnx("article:" + id + ":title", title,
+                "article:" + id + ":author", author, "article:" + id + ":time", time,
+                "article:" + id + ":content", content);
+
+        //每次发表长度记录长度
+        long length = jedis.strlen("article:" + id + ":content");
+        jedis.setnx("article:" + id + ":content_length", String.valueOf(length));
+    }
+
+    /**
+     * 查看一篇博客
+     *
+     * @param id
+     * @return
+     */
+    public List<String> getBlog(int id) {
+        List<String> blogResult = jedis.mget("article:" + id + ":title",
+                "article:" + id + ":author",
+                "article:" + id + ":time",
+                "article:" + id + ":content",
+                "article:" + id + ":content_length",
+                "article:" + id + ":like_count");
+        return blogResult;
+    }
+
+    /**
+     * 更新一篇博客
+     */
+    public void updateBlog(long id, String title, String content) {
+        jedis.mset("article:" + id + ":title", title, "article:" + id + ":content", content);
+
+        long length = jedis.strlen("article:" + id + ":content");
+        jedis.setnx("article:" + id + ":content_length", String.valueOf(length));
+    }
+
+    /**
+     * 预览博客
+     */
+    public String previewBlog(long id) {
+        return jedis.getrange("article:" + id + ":content", 0, 10);
+    }
+
+    /**
+     * 对博客点赞
+     *
+     * @param id
+     */
+    public void likeBlog(long id) {
+        jedis.incr("article:" + id + ":like_count");
+    }
 
     public static void main(String[] args) {
         BlogDemo demo = new BlogDemo();
-
-        long id = demo.getBlogId();
-
-        Map<String,String> blogMap = new HashMap<>();
-        blogMap.put("title","redis博客");
-        blogMap.put("content","redis内容");
-        blogMap.put("author","cheng");
-        blogMap.put("date","2020-11-12");
-        blogMap.put("id",String.valueOf(id));
-
-        demo.publishBlog(id,blogMap);
-        Map<String, String> blogResult = demo.findBlogById(id);
-        System.out.println(blogResult);
-
-        Map<String, String> updatedBlog = new HashMap<String, String>();
-        updatedBlog.put("title", "修改的Redistitle");
-        updatedBlog.put("content", "修改的Redis的内容");
-        demo.updateBlog(id,updatedBlog);
-        blogResult = demo.findBlogById(id);
-        System.out.println(blogResult);
-
-        demo.incrementBlogLikeAmount(id);
-        blogResult = demo.findBlogById(id);
-        System.out.println(blogResult);
-
-        blogResult = demo.findBlogById(id);
-        System.out.println(blogResult);
-    }
-
-    private static void incrementBlogLikeAmount(long id) {
-        jedis.hincrBy("article::"+id,"like",1);
-    }
-
-    private static Map<String, String> findBlogById(long id) {
-        jedis.hincrBy("article::"+id,"view",1);
-        return jedis.hgetAll("article::"+id);
-    }
-
-    private static void updateBlog(long id,Map<String, String> updatedBlog) {
-        jedis.hmset("article::"+id,updatedBlog);
-    }
-
-    private static void publishBlog(long id, Map<String, String> blogMap) {
-        if(jedis.hexists("article::"+id,"title")){
-            return;
-        }
-        jedis.hmset("article::"+id,blogMap);
-    }
-
-    private static long getBlogId() {
-        return jedis.incr("blog_id");
     }
 }
--- src/main/java/com/cheng/redisdemo/hash/ShortUrlDemo.java
+++ src/main/java/com/cheng/redisdemo/hash/ShortUrlDemo.java
@@ -3,55 +3,82 @@ package com.cheng.redisdemo.hash;
 import redis.clients.jedis.Jedis;
 
 /**
- * @Date:2020/11/13 14:33
+ * @Date:2020/12/30 18:10
  * @Author: Cheng
- * @Description:短地址网站demo
+ * @Description:短网址追踪案例
  */
 public class ShortUrlDemo {
 
-    private Jedis jedis;
-
     public ShortUrlDemo(){
-        jedis = new Jedis("127.0.0.1");
-        jedis.set("short_url_seed","123456789");
+        jedis.set("short_url_seed","100000000000");
     }
 
-    private static final String[] X36_ARRAY = "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(",");
+    private static final String X36 = "0123456789ABCDEFGHIJKLMNOBQRSTUVWXYZ";
+    private static final String[] X36_Array = "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,B,Q,R,S,T,U,V,W,X,Y,Z".split(",");
 
-    public static void main(String[] args) {
-        ShortUrlDemo demo = new ShortUrlDemo();
+    private Jedis jedis = new Jedis("127.0.0.1");
 
-        String shortUrl = demo.getShortUrl();
-        System.out.println(shortUrl);
-
-        for (int i = 0; i < 100; i++) {
-            demo.increatShortUrl(shortUrl);
+    public String getShortUrl(String longUrl) {
+        Long num = jedis.incr("short_url_seed");
+        StringBuffer sb = new StringBuffer();
+        while (num > 0) {
+            sb.append(X36_Array[(int) (num % 36)]);
+            num /= 36;
         }
+        String shortUrl = sb.reverse().toString();
 
-        long count = demo.getShortUrlCount(shortUrl);
-        System.out.println(count);
+        //初始化到redis中这个短连接的次数为0
+        //short_url_access_count代表map
+        //short_url_access_count: {
+        //	http://t.cn/XsGGA9d: 152,
+        //	http://t.cn/I93yUUaF: 269
+        //}
+        jedis.hset("short_url_access_count",shortUrl,"0");
+        //维护短连接和长连接
+        jedis.hset("url_mapping",shortUrl,longUrl);
+
+        return shortUrl;
     }
 
-    private long getShortUrlCount(String shortUrl) {
+    /**
+     * 点击短连接==>对短连接访问次数增长
+     * @param shortUrl
+     */
+    public void incrShortUrlAccessCount(String shortUrl){
+        jedis.hincrBy("short_url_access_count",shortUrl,1);
+    }
+
+    /**
+     * 获取短连接地址的访问次数
+     * @param shortUrl
+     * @return
+     */
+    public Long getShortUrlCount(String shortUrl){
         return Long.valueOf(jedis.hget("short_url_access_count",shortUrl));
     }
 
-    private void increatShortUrl(String shortUrl) {
-        jedis.hincrBy("short_url_access_count",shortUrl,1);
+    /**
+     * 根据短连接获取长连接
+     * @param shortUrl
+     * @return
+     */
+    public String getLongUrlCount(String shortUrl){
+        return jedis.hget("url_mapping",shortUrl);
     }
 
-    private String getShortUrl() {
-        long shortUrlSeed = jedis.incr("short_url_seed");
+    public static void main(String[] args) throws Exception{
+        ShortUrlDemo demo = new ShortUrlDemo();
+        String shortUrl = demo.getShortUrl("http://redis.com/index.html?dfd=sf&dfd=sf&dfd=sf&dfd=sf&dfd=sf&");
+        System.out.println("短连接地址："+shortUrl);
 
-        StringBuilder sb = new StringBuilder();
-        while (shortUrlSeed>0){
-            sb.append(X36_ARRAY[(int)(shortUrlSeed%36)]);
-            shortUrlSeed/=36;
+        //点击访问短连接
+        for (int i = 0; i < 129; i++) {
+            demo.incrShortUrlAccessCount(shortUrl);
         }
 
-        String shortUrl = sb.reverse().toString();
-        jedis.hincrBy("short_url_access_count",shortUrl,1);
-        jedis.hincrBy("url_mapper",shortUrl,1);
-        return shortUrl;
+        Long accessCount = demo.getShortUrlCount(shortUrl);
+        System.out.println("短连接访问次数："+accessCount);
+
+        System.out.println("短连接对应长连接："+demo.getLongUrlCount(shortUrl));
     }
 }
